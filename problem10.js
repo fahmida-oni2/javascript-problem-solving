e6b21d44e9f9b1943474402ae1fa5c8a47b825f9
fahmida-oni2 <onifahmida@gmail.com>
2026-01-08 08:20:38 +0600

    added the changes

--- problem10.js
+++ problem10.js
@@ -3,16 +3,16 @@
 const input =20
 
 for(let i=1 ; i<=20 ; i++){
-    if(i%3===0){
-        console.log("Ping") 
-    }
-    else if(i%5===0){
-        console.log("Pong") 
-    }
-    else if (i%3===0 && i%5===0){
-       console.log("PingPong") 
-    }
-    else{
-       console.log(i)
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("PingPong")
+    } 
+    else if (i % 3 === 0) {
+        console.log("Ping")
+    } 
+    else if (i % 5 === 0) {
+        console.log("Pong")
+    } 
+    else {
+        console.log(i);
     }
 }
